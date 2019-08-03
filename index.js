176a553dda249c47cd60012742b4b58dbd18513b
Muhammad Bhatti <muhammad.i.b@outlook.com>
2019-08-03 14:58:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 // add solution here
 
-var musiciansarray = [];
-var instrumentsarray = [];
 var theBeatlesarray = [];
 
 function theBeatlesPlay(musicians, instruments) {
   for (i = -1; i < musicians.length; i++) {
   theBeatlesarray.push(`${musicians[i]} plays ${instruments[i]}`);
-}
+  }
 }
 
