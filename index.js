5844c85c940070d2fd1c3a8960301aba36e6a2c8
Muhammad Bhatti <muhammad.i.b@outlook.com>
2019-08-03 14:56:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ function theBeatlesPlay(musicians, instruments) {
   
 }
 
-for (i = -1, i < musiciansarray.length, i++) {
+for (i = -1; i < musiciansarray.length; i++) {
   return `${musiciansarray[i]} plays ${instrumentsarray[i]}`;
 }
