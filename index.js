66c1a4f8e81ef154520153e956395dbacd53a5c9
Muhammad Bhatti <muhammad.i.b@outlook.com>
2019-08-03 14:56:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 // add solution here
 
 var musiciansarray = [];
+var instrumentsarray = [];
+var theBeatlesarray = [];
 
 function theBeatlesPlay(musicians, instruments); {
   
 }
 
-for (i = 0, i )
+for (i = -1, i < musiciansarray.length, i++) {
+  return `${musiciansarray[i]} plays ${instrumentsarray[i]`;
+}
